47e83eb6eb6eec9662b078e5c389804cbafd9cb1
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-03-11 17:40:01 -0300

    exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pastorzinho");
+Console.WriteLine();
+Console.WriteLine("Dó, ré, mi, fá, fá, fá");
+
+Console.Beep(1320,200);
+
+Console.Beep(1485,200);
+
+Console.Beep(1650,200);
+
+Console.Beep(1759,500);
+
+Console.Beep(1759,200);
+
+Console.Beep(1759,200);
+Console.WriteLine("Dó, ré, dó, ré, ré, ré");
+Console.Beep(1320,200);
+
+Console.Beep(1485,200);
+
+Console.Beep(1320,200);
+
+Console.Beep(1485,500);
+
+Console.Beep(1485,200);
+
+Console.Beep(1485,200);
+Console.WriteLine("Dó, sol, fá mi, mi, mi");
+Console.Beep(1320,200);
+
+Console.Beep(1980,200);
+
+Console.Beep(1759,200);
+
+Console.Beep(1650,500);
+
+Console.Beep(1650,200);
+
+Console.Beep(1650,200);
+Console.WriteLine("Dó, ré, mi, fá, fá, fá");
+Console.Beep(1320,200);
+
+Console.Beep(1485,200);
+
+Console.Beep(1650,200);
+
+Console.Beep(1759,500);
+
+Console.Beep(1759,200);
+
+Console.Beep(1759,200);
+
